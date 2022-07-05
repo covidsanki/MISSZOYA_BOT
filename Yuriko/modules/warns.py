223e18987e4e78ed8f1704cee5d4a845cc35169e
covidsanki <106461308+covidsanki@users.noreply.github.com>
2022-07-05 16:55:26 +0530

    Update warns.py

--- Yuriko/modules/warns.py
+++ Yuriko/modules/warns.py
@@ -509,7 +509,7 @@ __help__ = """
 
 ✗ /strongwarn - `<on/yes/off/no> If set to on, exceeding the warn limit will result in a ban. Else, will just punch.`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: HᴅFᴀᴍᴀɪʟʏ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: @SWAGxHOUSExCHAT!*
 """
 
 __mod_name__ = "Wᴀʀɴɪɴɢ"
